8cbeb6b5122f7b04b349f4fa957546fdc2cef787
xu1718191411 <xu1718191411@gmail.com>
2019-12-02 00:31:21 +0900

    forward

--- CONTEXT_137/summer_vacation.py
+++ CONTEXT_137/summer_vacation.py
@@ -1,32 +1,52 @@
-def calculate(arr,M):
-    sum = 0
-    for i in range(M):
-        index = 0
-        value = 0
-        for j in range(len(arr)):
-            lastDay = i + arr[j][0]
-            if lastDay <= M:
-                if arr[j][1] > value:
-                    index = j
-                    value = arr[j][1]
+from heapq import heappop, heappush
+import sys
+input = sys.stdin.readline
 
-                if (arr[j][1] == value) and (arr[index][0] < arr[j][0]):
-                    index = j
-                    value = arr[j][1]
+def calculate(N,M,arr):
+    ret = 0
 
-        if len(arr) > 0:
-            sum += value
-            arr.pop(index)
+    j = 0
 
-        if len(arr) == 0:
-            break
+    res = []
 
-    return sum
+    # 1,2,3,4,5
+    #         ↑
+    # 4,3,2,1
 
-N, M = map(int, input().split())
-arr = []
-for i in range(N):
-    day, mount = map(int, input().split())
-    arr.append([day,mount])
-result = calculate(arr,M)
+    # M = 4
+    # range(1,5) => 1,2,3,4
+
+    for i in range(1,M+1):
+        # i = 1,2,3
+        # 比如M=4的话，第三天(最多1天),第二天(最多2天),第一天(最多3天)
+
+        # 记下来循环打工信息资源,以j为标记
+        # j为资源编号
+        # arr[j][0]为该编号资源的天数
+
+
+        # 候补的资源，满足所有天数，因为是满足后续的天数来算的
+
+        while (j < N) and (arr[j][0] <= i):
+
+                heappush(res, -arr[j][1])
+                j = j + 1 #j找到的话就往上顶一格，一共这一天，或者前一天寻找的时候以下一个资源天为开始
+
+        if len(res) > 0:
+            ret -= heappop(res)
+
+    return ret
+
+
+N,M = map(int,input().split())
+
+
+# 后面的逻辑是基于前面的从小到大的排序的
+list = [list(map(int,input().split())) for i in range(N)]
+
+arr = sorted(list,key=lambda x:x[0])
+
+result = calculate(N,M,arr)
+
 print(result)
+

dedde4d62fe54b2cdea54482dd77bf0424c1c415
xu1718191411 <xu1718191411@gmail.com>
2019-12-14 09:24:12 +0900

    simple

--- CONTEXT_133/rain_flows_into_dams.py
+++ CONTEXT_133/rain_flows_into_dams.py
@@ -1,25 +1,28 @@
-def calculate(arr,S):
-
+def calculate(N, arr, S):
     result = []
-    even = 0
+    tmp = 0
     for i in range(len(arr)):
+        if i % 2 == 1:
+            tmp += arr[i] * 2
 
-        if i % 2 == 0:
-            even = S-calculate2(i+1,arr[i+1:])
-            result.append(even)
-        else:
-            odd = arr[i-1]*2-even
-            result.append(odd)
-            S = S - even - odd
+    x0 = S - tmp
+
+    result.append(x0)
+
+    for i in range(1, N):
+        result.append(arr[i - 1] * 2 - result[i - 1])
 
     return result
 
-def calculate2(i,arr):
+
+def calculate2(i, arr):
     # 取偶数项
-    return sum(arr[::2])*2
+    return sum(arr[::2]) * 2
+
 
 def calculateTotaAmount(arr):
     return sum(arr)
+
 
 def printArr(arr):
     result = ""
@@ -35,8 +38,5 @@ N = int(input())
 S = input().split(" ")
 aArr = [int(s) for s in S]
 
-
-result = calculate(aArr,calculateTotaAmount(aArr))
-
+result = calculate(N, aArr, calculateTotaAmount(aArr))
 print(" ".join([str(s) for s in result]))
-

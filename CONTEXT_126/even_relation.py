8f825b8347f818c105e76e1f33169617658b1b15
xu1718191411 <xu1718191411@gmail.com>
2020-01-13 15:48:47 +0900

    forward sys.setrecursionlimit(20000000)!!!!!!

--- CONTEXT_126/even_relation.py
+++ CONTEXT_126/even_relation.py
@@ -1,3 +1,5 @@
+import sys
+sys.setrecursionlimit(20000000)
 def prepare(n,arr):
     nodeArr = [[] for i in range(n)]
     for ar in arr:
@@ -5,20 +7,15 @@ def prepare(n,arr):
         nodeArr[ar[1]-1].append([ar[0]-1,ar[2]])
     return nodeArr
 
-def dfs(parent,currentNode,distance):
-
-    if len(nodeArr[currentNode]) == 1:
-        if currentNode != 0:
-            # 末点
-            res[currentNode] = distance
-            return
-
+def dfs(currentNode,distance):
+    res[currentNode] = distance
     for next in nodeArr[currentNode]:
         nextNode = next[0]
         nextNodeDistance = next[1]
-        if nextNode != parent:
-            dfs(currentNode,nextNode,distance + nextNodeDistance)
-    res[currentNode] = distance
+        if res[nextNode] == -1:
+            dfs(nextNode,distance + nextNodeDistance)
+
+
 
 
 n = int(input())
@@ -36,7 +33,7 @@ if n == 1:
 else:
     res = [-1 for i in range(n)]
 
-    dfs(-1,0,0)
+    dfs(0,0)
 
     result = [(re % 2) for re in res]
 

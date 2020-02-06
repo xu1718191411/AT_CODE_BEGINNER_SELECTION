ca396c48826a65c47ad83692c0e5325f85e8873c
xu1718191411 <xu1718191411@gmail.com>
2020-02-06 20:57:19 +0900

    forward

--- CONTEXT_146/coloring_edges_on_tree.py
+++ CONTEXT_146/coloring_edges_on_tree.py
@@ -1,42 +1,53 @@
-n = 8
-arr = [
-    [1, 2],
-    [2, 3],
-    [2, 4],
-    [2, 5],
-    [4, 7],
-    [5, 6],
-    [6, 8]
-]
+n = int(input())
+arr = []
+for i in range(n-1):
+    arr.append([int(s) for s in input().split(" ")])
 
 childNodeNums = [0 for i in range(n)]
 childNodeStatus = [-1 for i in range(n)]
-lineColors = [0 for i in range(n-1)]
+nodeLines = [0 for i in range(n - 1)]
+
 
 def prepare(n, arr):
     s = [[] for i in range(n)]
     for ar in arr:
-        s[ar[0]-1].append(ar[1] - 1)
-        s[ar[1]-1].append(ar[0] - 1)
+        s[ar[0] - 1].append(ar[1] - 1)
+        s[ar[1] - 1].append(ar[0] - 1)
     return s
 
-def dfs(currentNode,arr):
+
+# x为记录入边的颜色
+def dfs(currentNode, arr, x):
+    m1 = childNodeNums
+    m2 = childNodeStatus
     childNodes = arr[currentNode]
     if len(childNodes) == 0:
         return
 
     childNodeNums[currentNode] = childNodeNums[currentNode] + len(arr[currentNode])
-
     childNodeStatus[currentNode] = 1
 
-    for childNodeIndex in childNodes:
-        if childNodeStatus[childNodeIndex] == -1:
-            dfs(childNodeIndex, arr)
+    kk = 0
+    for i, childNodeIndex in enumerate(childNodes):
 
-def order(root,num,data):
-    print(1)
+        if childNodeStatus[childNodeIndex] == -1:
+            kk = kk + 1
+            if x == kk:
+                kk = kk + 1
+            appendIntoNode(currentNode+1,childNodeIndex+1,kk)
+            dfs(childNodeIndex, arr, kk)
+
+
+def appendIntoNode(currentNode,childNode,value):
+    for i,v in enumerate(arr):
+        if (v[0] == currentNode and v[1] == childNode):
+            nodeLines[i] = value
+
 
 data = prepare(n, arr)
-dfs(3,data)
+dfs(0, data, -1)
 
-print(childNodeNums)
+print(max(childNodeNums))
+
+for ar in nodeLines:
+    print(ar)

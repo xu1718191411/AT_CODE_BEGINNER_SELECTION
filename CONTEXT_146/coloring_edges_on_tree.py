365bf65200d24b0684fb6dbed2e4b173498bf47b
xu1718191411 <xu1718191411@gmail.com>
2020-02-11 12:15:27 +0900

    forward

--- CONTEXT_146/coloring_edges_on_tree.py
+++ CONTEXT_146/coloring_edges_on_tree.py
@@ -3,55 +3,51 @@ setrecursionlimit(100000)
 
 n = int(input())
 arr = []
-for i in range(n - 1):
-    arr.append([int(s) for s in input().split(" ")])
 
-mmk = {}
-
-childNodeNums = [0 for i in range(n)]
-childNodeStatus = [-1 for i in range(n)]
-
-
-def generateID(fr, to):
-    if to < fr:
-        fr, to = to, fr
-    return fr * 100000 + to
-
+for i in range(n-1):
+    S = input()
+    ar = [int(s) for s in S.split(" ")]
+    arr.append(ar)
 
 def prepare(n, arr):
-    s = [[] for i in range(n)]
+    data = [[] for i in range(n)]
+    nodeStates = [-1 for i in range(n)]
+    childNums = [0 for i in range(n)]
+
     for ar in arr:
-        s[ar[0] - 1].append(ar[1] - 1)
-        s[ar[1] - 1].append(ar[0] - 1)
-    return s
+        start = ar[0] - 1
+        to = ar[1] - 1
+        data[start].append(to)
+        data[to].append(start)
+    return data, nodeStates, childNums
 
 
-# x为记录入边的颜色
+data, nodeStates, childNums = prepare(n, arr)
+mmk = {}
+def generateID(start,to):
+    if start < to:
+        start,to = to, start
+
+    return start * 100000 + to
+
 def dfs(currentNode, arr, x):
     childNodes = arr[currentNode]
-    if len(childNodes) == 0:
-        return
-
-    childNodeNums[currentNode] = childNodeNums[currentNode] + len(arr[currentNode])
-    childNodeStatus[currentNode] = 1
-
+    nodeStates[currentNode] = 1
+    childNums[currentNode] = len(childNodes)
     kk = 0
-    for i, childNodeIndex in enumerate(childNodes):
-
-        if childNodeStatus[childNodeIndex] == -1:
+    for ch in childNodes:
+        if nodeStates[ch] == -1:
             kk = kk + 1
-            if x == kk:
+            if kk == x:
                 kk = kk + 1
-            mmk[generateID(currentNode + 1,childNodeIndex + 1)] = kk
-            dfs(childNodeIndex, arr, kk)
+            mmk[generateID(currentNode+1,ch+1)] = kk
+            dfs(ch, arr, kk)
 
-
-data = prepare(n, arr)
 dfs(0, data, -1)
-
-print(max(childNodeNums))
-
+print(max(childNums))
 for ar in arr:
-    a0 = ar[0]
-    a1 = ar[1]
-    print(mmk[generateID(a0,a1)])
+    start = ar[0]
+    to = ar[1]
+    print(mmk[generateID(start,to)])
+
+

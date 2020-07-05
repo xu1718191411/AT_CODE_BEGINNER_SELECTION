393d0a7d2cab8fdd3eafd11c5aa467ce2153c458
xuzhongwei <xu1718191411@gmail.com>
2020-07-05 20:57:07 +0900

    forward

--- CONTEXT_148/double_factorial.py
+++ CONTEXT_148/double_factorial.py
@@ -1,9 +1,17 @@
-n = 5
-
 def calculate(n):
-    a = n // 10
-    b = n // 50
+    if n % 2 == 1:
+        return 0
 
-    print(a + b)
+    s1 = n // 10
 
-calculate(n)
+    index = 1
+
+    s2 = 0
+    while (5 ** index) <= s1:
+        s2 += (s1 // (5 ** index))
+        index += 1
+
+    return s1 + s2
+
+
+print(calculate(int(input())))

013449bccdceabb5561aace46c91883f8f559dfa
xu1718191411 <xu1718191411@gmail.com>
2020-07-25 12:15:04 +0900

    forward

--- CONTEXT_10/c.py
+++ CONTEXT_10/c.py
@@ -9,22 +9,22 @@ import math
 # ]
 
 
-txa, tya, txb, tyb, t, v = map(int,input().split())
+txa, tya, txb, tyb, t, v = map(int, input().split())
 n = int(input())
 arr = []
 
 for i in range(n):
-    arr.append(list(map(int,input().split())))
+    arr.append(list(map(int, input().split())))
 
 
 def calculate(txa, tya, txb, tyb, n, arr, maxDistance):
-
     isPossible = False
     for i in range(n):
         dx = arr[i][0]
         dy = arr[i][1]
 
-        distance = math.sqrt(pow((dx - txa), 2) + pow((dy - tya), 2)) + math.sqrt(pow((dx - txb), 2) + pow((dy - tyb), 2))
+        distance = math.sqrt(pow((dx - txa), 2) + pow((dy - tya), 2)) + math.sqrt(
+            pow((dx - txb), 2) + pow((dy - tyb), 2))
 
         if maxDistance >= distance:
             isPossible = True
@@ -33,5 +33,6 @@ def calculate(txa, tya, txb, tyb, n, arr, maxDistance):
         print("YES")
     else:
         print("NO")
+
 
 calculate(txa, tya, txb, tyb, n, arr, t * v)
